9b52abd149db3a69087b129534e51b117b64b2e2
Matthew Cacutt <matthew.cacutt@gmail.com>
2025-11-09 14:36:40 +0000

    SEO

---
+++ client/GenerateSitemap.js
@@ -0,0 +1,34 @@
+
+import { SitemapStream, streamToPromise } from 'sitemap';
+import { createWriteStream } from 'fs';
+
+const BASE_URL = 'https://ktanewhocansolve.com';
+
+const staticRoutes = [
+  '/',
+  '/users',
+  '/bombs',
+  '/practice',
+  '/missions/sample-mission', 
+  '/profile/sample-user',    
+];
+
+async function generateSitemap() {
+  const sitemap = new SitemapStream({ hostname: BASE_URL });
+  const writeStream = createWriteStream('./public/sitemap.xml');
+
+  staticRoutes.forEach((route) => {
+    sitemap.write({
+      url: route,
+      changefreq: 'weekly',
+      priority: route === '/' ? 1.0 : 0.8,
+    });
+  });
+
+  sitemap.end();
+
+  const data = await streamToPromise(sitemap);
+  writeStream.write(data.toString());
+}
+
+generateSitemap();
